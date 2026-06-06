13a356adfd919db7da63e04db49748ee25e2404e
Dvhlong55 <hoanglong251027@gmail.com>
2026-06-06 17:11:42 +0700

    Update index.html recent posts and featured post; update sidebar counter

--- core/js/sidebar-data.js
+++ core/js/sidebar-data.js
@@ -2,7 +2,7 @@ const LEFT_TAGS_HTML = `
         <div class="side-widget fade-up">
             <ul class="side-widget-list">
                 <li><a href="/SMP/pages/toanhoc.html?filter=vmo">VMO <span>7</span></a></li>
-                <li><a href="/SMP/pages/toanhoc.html?filter=hsg">HSG & Chuyên Toán 9 <span>18</span></a></li>
+                <li><a href="/SMP/pages/toanhoc.html?filter=hsg">HSG & Chuyên Toán 9 <span>21</span></a></li>
                 <li><a href="/SMP/pages/toanhoc.html?filter=thpt">THPT <span>0</span></a></li>
                 <li><a href="/SMP/pages/toanhoc.html?filter=uni">Đại Học <span>1</span></a></li>
                 <li><a href="/SMP/pages/toanhoc.html?filter=challenge">Thách Thức Kì Này <span>1</span></a></li>
