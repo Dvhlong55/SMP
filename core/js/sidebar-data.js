77e6e33f6e82a1d0092624834216e6f9379d2c85
hoang <hoang@example.com>
2026-06-20 12:05:22 +0700

    Update registration link, add post to toanhoc.html VMO category, and increment VMO sidebar count

--- core/js/sidebar-data.js
+++ core/js/sidebar-data.js
@@ -1,7 +1,7 @@
 const LEFT_TAGS_HTML = `
         <div class="side-widget fade-up">
             <ul class="side-widget-list">
-                <li><a href="/pages/toanhoc.html?filter=vmo">VMO <span>12</span></a></li>
+                <li><a href="/pages/toanhoc.html?filter=vmo">VMO <span>13</span></a></li>
                 <li><a href="/pages/toanhoc.html?filter=thcs">THCS <span>35</span></a></li>
                 <li><a href="/pages/toanhoc.html?filter=thpt">THPT <span>1</span></a></li>
                 <li><a href="/pages/toanhoc.html?filter=uni">Đại Học <span>1</span></a></li>
